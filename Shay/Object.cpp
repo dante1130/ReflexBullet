31c2783648528e158e4adeaf60341424ef0b9315
openGLBRUH <SpencerShawJoe@outlook.com>
2021-09-17 09:15:06 +0800

    Allowed objects with faces of varying polygon size

--- Shay/Object.cpp
+++ Shay/Object.cpp
@@ -7,6 +7,10 @@ Object3D::~Object3D()
 {
 	vertices.clear();
 	texCoord.clear();
+	for (int i = 0; i < GetFaceVectorSize(); ++i)
+	{
+		faces[i].clear();
+	}
 	faces.clear();
 }
 
@@ -20,44 +24,14 @@ void Object3D::AddCoord(const glm::vec2& coord)
 	texCoord.push_back(coord);
 }
 
-void Object3D::AddVertexNormal(const glm::vec3& vn)
+void Object3D::AddFaces(const std::vector<FaceBase>& face_temp)
 {
-	vertexNormals.push_back(vn);
-}
-
-void Object3D::AddFaces(const FaceBase face_arr[4])
-{
-	std::array<FaceBase, 4> too;
-
-	for (unsigned i = 0; i < 4; ++i) {
-		too[i] = face_arr[i];
-	}
-	faces.push_back(too);
+	faces.push_back(face_temp);
 }
 
 void Object3D::SetTextureName(const std::string& fileName)
 {
 	textureFile = fileName;
-}
-
-void Object3D::SetMTLName(const std::string& mtlName)
-{
-	mtlFile = mtlName;
-}
-
-void Object3D::SetMTLArrayLocation(int location)
-{
-	mtlLoc = location;
-}
-
-int Object3D:: GetMTLArrayLocation()
-{
-	return mtlLoc;
-}
-
-const std::string& Object3D::GetMTLName()
-{
-	return mtlFile;
 }
 
 const glm::vec3& Object3D::GetVertex(unsigned i) const
@@ -70,12 +44,7 @@ const glm::vec2& Object3D::GetCoord(unsigned i) const
 	return texCoord[i];
 }
 
-const glm::vec3& Object3D::GetVertexNormal(unsigned i) const
-{
-	return vertexNormals[i];
-}
-
-const std::array<FaceBase, 4>& Object3D::GetFace(unsigned i) const
+const std::vector<FaceBase>& Object3D::GetFace(unsigned i) const
 {
 	return faces[i];
 }
@@ -85,29 +54,19 @@ const std::string& Object3D::GetName() const
 	return textureFile;
 }
 
-const std::vector<glm::vec3>& Object3D::GetVertexVector() const
+const unsigned Object3D::GetFaceVectorSize() const
 {
-	return vertices;
-}
-
-const std::vector<glm::vec2>& Object3D::GetCoordVector() const
-{
-	return texCoord;
-}
-
-const std::vector<std::array<FaceBase, 4>>& Object3D::GetFaceVector() const
-{
-	return faces;
+	return faces.size();
 }
 
 
 void Object3D::DisplayObject(int textureID)
 {
 	glBindTexture(GL_TEXTURE_2D, tp.GetTexture(textureID));
-	for (unsigned i = 0; i < GetFaceVector().size(); ++i)
+	for (unsigned i = 0; i < GetFaceVectorSize(); ++i)
 	{
 		glBegin(GL_POLYGON);
-		for (unsigned j = 0; j < 4; ++j)
+		for (unsigned j = 0; j < GetFace(i).size(); ++j)
 		{
 			unsigned temp_v = GetFace(i)[j].v - 1;
 			unsigned temp_vt = GetFace(i)[j].vt - 1;
@@ -116,6 +75,6 @@ void Object3D::DisplayObject(int textureID)
 		}
 		glEnd();
 	}
-
+	
 	return;
 }
--- Shay/Object.h
+++ Shay/Object.h
@@ -15,8 +15,8 @@
  */
 struct FaceBase
 {
-	/// stores vertices/texture coordinate position/vertex normal from .obj file
-	int v, vt, vn; 
+	/// stores vertices/texture coordinate position from .obj file
+	int v, vt; 
 };
 
 /**
@@ -51,18 +51,11 @@ public:
 	void AddCoord(const glm::vec2& coord); 
 
 	/**
-	* @brief	Stores the vertex normals
-	* @param	vn	- Vertex normal
-	* @return	Void
-	*/
-	void AddVertexNormal(const glm::vec3& vn);
-
-	/**
 	 * @brief Creates faces using FaceBase array.
 	 * @param face_arr [4] const FaceBase
 	 * @return void
 	 */
-	void AddFaces(const FaceBase face_arr[4]); 
+	void AddFaces(const std::vector<FaceBase>& face_temp);
 
 	/**
 	 * @brief Saves the texture name to program.
@@ -70,34 +63,6 @@ public:
 	 * @return void
 	 */
 	void SetTextureName(const std::string& fileName); 
-
-	/**
-	* @brief	Assigns an object an mtl file name
-	* @param	mtlName	- The name of the mtl file
-	* @return	Void
-	*/
-	void SetMTLName(const std::string& mtlName);
-	
-	/**
-	* @brief	Returns the mrl file name associated with the object
-	* @param	No param
-	* @return	string&	- THe MTL file name
-	*/
-	const std::string& GetMTLName();
-
-	/**
-	* @brief	Assigns which material the object uses
-	* @param	location	- The location in the material array that the object uses
-	* @return	Void
-	*/
-	void SetMTLArrayLocation(int location);
-
-	/**
-	* @brief	Used to find the material properties for an object
-	* @param	No param
-	* @return	int		- returns an index for a material array which references the materials properties
-	*/
-	int GetMTLArrayLocation();
 
 	/**
 	 * @brief Returns a vertex of index i.
@@ -114,44 +79,17 @@ public:
 	const glm::vec2& GetCoord(unsigned i) const; 
 
 	/**
-	* @brief	Returns the vertex normal of a vertex based on index i
-	* @param	i	- The index of the vertex
-	* @return	const glm::vec3&	- The normal of a vertex
-	*/
-	const glm::vec3& GetVertexNormal(unsigned i) const;
-
-
-
-	/**
 	 * @brief Returns a set of vertex and coordinate pairs of index i.
 	 * @param i unsigned
 	 * @return const std::array<Facebase, 4>&
 	 */
-	const std::array<FaceBase, 4>& GetFace(unsigned i) const;
+	const std::vector<FaceBase>& GetFace(unsigned i) const;
 
 	/**
 	 * @brief Returns the name of the texture file.
 	 * @return const std::string&
 	 */
 	const std::string& GetName() const;
-
-	/**
-	 * @brief Allows access to the contents of vertices vector.
-	 * @return const std::vector<glm::vec3>&
-	 */
-	const std::vector<glm::vec3>& GetVertexVector() const; 
-
-	/**
-	 * @brief Allows access to the contents of texCoord vector.
-	 * @return const std::vector<glm::vec2>&
-	 */
-	const std::vector<glm::vec2>& GetCoordVector() const;
-
-	/**
-	 * @brief Allows access to the contents of faces vector.
-	 * @return const std::vector<std::array<Facebase,4>>&
-	 */
-	const std::vector<std::array<FaceBase, 4>>& GetFaceVector() const;
 
 	/**
 	* @brief	Displays the stored object based on the texture
@@ -161,20 +99,21 @@ public:
 	void DisplayObject(int textureID);
 
 private:
+	/**
+	 * @brief Allows access to the contents of faces vector.
+	 * @return const std::vector<std::array<Facebase,4>>&
+	 */
+	const unsigned GetFaceVectorSize() const;
+
+
 	/// stores the vertices
 	std::vector<glm::vec3> vertices; 
 	/// stores the texture coordinates
 	std::vector<glm::vec2> texCoord; 
-	/// stores the vertex normals
-	std::vector<glm::vec3> vertexNormals;
 	/// saves the links to v and vt for faces
-	std::vector<std::array<FaceBase, 4>> faces; 
+	std::vector<std::vector<FaceBase>> faces; 
 	/// stores image name (and extension)
 	std::string textureFile; 
-	/// The name of the mtl file (for material properties for lighting)
-	std::string mtlFile;
-	/// Points to the index of the material properties of the object
-	int mtlLoc;
 };
 
 #endif
